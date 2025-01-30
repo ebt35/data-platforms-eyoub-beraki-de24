115ae3f12e358e145e8fef62d4243d916d79d068
ebt35 <eyoub.beraki@hotmail.com>
2025-01-30 10:07:50 +0100

    code alongs starting

--- code_alongs/05_dockerfile/main.py
+++ code_alongs/05_dockerfile/main.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-import pandas as pd
+import pandas as pd 
 
 data_path = Path(__file__).parent / "data"
 
 print(data_path / "prog_book.csv")
 
+df = pd.read_csv(data_path / "prog_book.csv")
+print(df.head())
 
-
+print(df.info())
 
