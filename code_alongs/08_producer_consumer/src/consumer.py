c889f2fd3d24c23293a81c085d1903c41a60c20a
ebt35 <eyoub.beraki@hotmail.com>
2025-02-03 01:10:21 +0100

    consumer

--- code_alongs/08_producer_consumer/src/consumer.py
+++ code_alongs/08_producer_consumer/src/consumer.py
@@ -11,13 +11,13 @@ jokes_topic = app.topic(name="jokes", value_deserializer="json")
 sdf = app.dataframe(topic=jokes_topic)
 
 # def print_message(message):
-    # print(message)
-    # sdf.update(print_message)
-sdf.update(lambda message: print(message))
+#     print(message)
+# sdf.update(print_message)
+sdf.update(lambda message: print(f"Input message: {message}"))
 
-
+# transformations
 def split_words(message):
-    return [{"word": word} for word in ["joke_text"].split()]
+    return [{"word": word} for word in message["joke_text"].split()]
 
 
 sdf = sdf.apply(split_words, expand=True)
@@ -26,5 +26,5 @@ sdf["length"] = sdf["word"].apply(lambda word: len(word))
 
 sdf.update(lambda row: print(f"Output: {row}"))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
